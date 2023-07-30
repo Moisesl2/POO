f8a51a9933593fe515c63ea9d63d03fd1b5f408e
Moisesl2 <moiseshenrique080@gmail.com>
2023-07-30 15:35:32 -0300

    Baralho/main

--- Baralho.java
+++ Baralho.java
@@ -1,53 +1,75 @@
 import java.util.Scanner;
+public class Baralho{
+public static void main(String[] args) {
+    Scanner ent = new Scanner (System.in);
+    System.out.print("informe o valor da carta: ");
+    int valor = ent.nextInt();
+    System.out.print("Informe o naipe: ");
+    int naipe = ent.nextInt();
 
-public class Baralho {
-    public static void main(String[] args) {
-        Scanner ent = new Scanner(System.in);
+    switch (valor) {
+        case 1:
+            System.out.println("Ás de ");
+            break;
+        case 2:
+            System.out.println("dois de ");
+            break;
+        case 3:
+            System.out.println("tres de ");
+            break;
+        case 4:
+            System.out.println("quatro de ");
+            break;
+        case 5:
+            System.out.println("cinco de ");
+            break;        
+        case 6:
+            System.out.println("seis de ");
+            break;
+        case 7:
+            System.out.println("sete de ");
+            break;
+        case 8:
+            System.out.println("oito de ");
+            break;
+        case 9:
+            System.out.println("nove de ");
+            break;
+        case 10:
+            System.out.println("dez de ");
+            break;    
+        case 11:
+            System.out.println("valete de ");
+            break;
+        case 12:
+            System.out.println("rainha de ");
+            break;
+        case 13:
+            System.out.println("rei de ");
+            break;    
+         default:
+            System.out.println("Entrada invalida!");
+            break;
+    }
 
-        int numCarta, numNaipe;
+    switch (naipe) {
+        case 1:
+            System.out.println("paus");  
+            break;
+        case 2:
+            System.out.println("ouro");  
+            break;
+        case 3:
+            System.out.println("espada");  
+            break;
+        case 4:
+            System.out.println("copas");  
+            break;
+        default:
+            System.out.println("entrada invalida!");
+            break;
+    }
 
-        System.out.println("informe o valor da carta:  ");
-        numCarta = ent.nextInt();
-        System.out.println("1: Ouros ");
-        System.out.println("2: Paus ");
-        System.out.println("3: Copas ");
-        System.out.println("4: Espadas");
-        System.out.println("informe o naipe:");
-        numNaipe = ent.nextInt();
-
-        for(int i = 0; i <= numCarta;){
-            for (int j = 0; j < numNaipe;) {
-                
-        
-            }
-            if (numCarta == 0 && numNaipe == 4){
-                System.out.println();
-
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
-
-
-
-
-
-
-
-
-        ent.close();
+    ent.close();
     }
 }
